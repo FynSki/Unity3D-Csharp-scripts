6713deb5e7aa6a06407022bd84970d4b6ff63b85
FynSki <adam3768@gmail.com>
2020-11-18 09:51:59 +0100

    Example UI navigation Scripts

---
+++ infoDisplay.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class infoDisplay : MonoBehaviour
+{
+/*
+// ======================================== 
+//
+// info Setup Script
+// version 1.0.1
+// date - 2020-11-12
+// update - 
+// created by Adam Janiszewski
+				change log
+
+// ======================================== 
+*/
+
+// ========================================
+//	VARIABLES
+// ========================================  
+    public int infoWasWatched;
+    public GameObject infoUi;
+// ========================================
+//	START
+// ======================================== 
+    void Start()
+    {
+        loadPrefs();
+    }
+
+// ========================================
+//	UPDATE
+// ======================================== 
+    void Update()
+    {
+        
+    }
+
+// ========================================
+//	VARIABLES
+// ======================================== 
+   //Start Game or Info
+    public void changeSceenFunc(string gameSceneToChange)
+    { 
+        if(infoWasWatched == 1)
+        {
+            SceneManager.LoadScene(gameSceneToChange);
+            //PlayerPrefs.SetInt("infoPrefName" , 1);
+        }
+        else
+        {
+            infoUi.SetActive(true);
+        }
+        
+    }
+
+    public void assignePrefFunc(string gameSceneToChange)
+    {
+        PlayerPrefs.SetInt("infoPrefName" , 1);
+        SceneManager.LoadScene(gameSceneToChange);
+    }
+
+
+
+    public void loadPrefs()
+    {
+        if(PlayerPrefs.HasKey("infoPrefName"))
+            {
+                infoWasWatched = PlayerPrefs.GetInt("infoPrefName");
+            }
+    }
+
+// END SCRIPT
+}
---
+++ uiButton.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class uiButton : MonoBehaviour
+{
+ /* ========================================
+
+    UI Buttons Script
+    version 1.0.1
+    date - 2020-04-22
+    update - 2020-09-18
+    created by Adam Janiszewski
+
+    ===CHANGE LOG:===
+    1. dodanie zmiany UI
+
+    =================
+
+// ========================================
+*/ 
+// ========================================
+//	VARIABLES
+// ========================================
+   
+// ========================================
+//	FUNCTIONS
+// ========================================
+    public void changeSceen(string gameSceneToChange)
+    { 
+        SceneManager.LoadScene(gameSceneToChange);
+    }
+
+    public void ExitGame()
+	{
+		Application.Quit();
+	}
+
+    public void UiToActiveFunc(GameObject toActive)
+    {
+        toActive.SetActive(true);
+    }
+
+    public void UiToDeactiveFunc(GameObject toDeactive)
+    {
+        toDeactive.SetActive(false);
+    }
+
+
+}
